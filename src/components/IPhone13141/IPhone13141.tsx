7541fc73806fa79cb5803e12cbb113244cbecc87
arneyva <princethereal1416@gmail.com>
2024-04-15 14:21:37 +0700

    revisi

--- src/components/IPhone13141/IPhone13141.tsx
+++ src/components/IPhone13141/IPhone13141.tsx
@@ -2,7 +2,7 @@ import { memo } from 'react';
 import type { FC } from 'react';
 
 import resets from '../_resets.module.css';
-import classes from './IPhone13141.module.css';
+import classes from './IPhone13141.module.scss';
 
 interface Props {
   className?: string;
@@ -11,9 +11,18 @@ interface Props {
 export const IPhone13141: FC<Props> = memo(function IPhone13141(props = {}) {
   return (
     <div className={`${resets.clapyResets} ${classes.root}`}>
-      <div className={classes._95673652}></div>
+      {/* <div className={classes._95673652}></div>
       <div className={classes.PngtreePurpleEnvelopeLoveEnvel}></div>
-      <div className={classes.PngtreePurpleEnvelopeLoveEnvel2}></div>
+      <div className={classes.PngtreePurpleEnvelopeLoveEnvel2}></div> */}
+      <div className={classes._95673652}>
+        <img src="./assets/_95673652.png" alt="" />
+      </div>
+      <div className={classes.PngtreePurpleEnvelopeLoveEnvel}>
+        <img src="./assets/PngtreePurpleEnvelopeLoveEnvel.png" alt="" />
+      </div>
+      <div className={classes.PngtreePurpleEnvelopeLoveEnvel2}>
+        <img src="./assets/PngtreePurpleEnvelopeLoveEnvel.png" alt="" />
+      </div>
     </div>
   );
 });
